e989aecfc752602ce0ba8035dafd7a008f5a5edd
alfredpoly <36860113+Alfredpoly@users.noreply.github.com>
2022-10-20 16:26:14 +0200

    2nd

--- ex02/src/ShrubberyCreationForm.cpp
+++ ex02/src/ShrubberyCreationForm.cpp
@@ -1,10 +1,10 @@
 #include "ShrubberyCreationForm.hpp"
 // Constructor initializes attributes to 0 by default 
 ShrubberyCreationForm::ShrubberyCreationForm()
-	: _target(0), Form("ShrubberyCreationForm", 145, 137)
+	: _target("default"), Form("ShrubberyCreationForm", 145, 137)
 {
-	std::cout << "ShrubberyCreationForm has been constructed" << std::endl;
-	
+	std::cout << "Default ShrubberyCreationForm has been constructed" << std::endl;
+
 }
  
 ShrubberyCreationForm::ShrubberyCreationForm( const ShrubberyCreationForm& rhs)
@@ -13,8 +13,9 @@ ShrubberyCreationForm::ShrubberyCreationForm( const ShrubberyCreationForm& rhs)
 }
  
 ShrubberyCreationForm::ShrubberyCreationForm(std::string newTarget) 
-	: _target(newTarget)
+	: _target(newTarget),Form("ShrubberyCreationForm", 145, 137)
 {
+	std::cout << _target << " ShrubberyCreationForm has been constructed" << std::endl;
 
 }
 
@@ -33,10 +34,4 @@ std::string ShrubberyCreationForm::getTarget() { return _target; }
  
 // Setters 
 void ShrubberyCreationForm::setTarget(std::string newTarget) { _target = newTarget; }
- 
-// Output
-void ShrubberyCreationForm::output()
-{
-  std::cout << "target : " << _target << std::endl; 
-}
- 
+
--- ex02/src/ShrubberyCreationForm.hpp
+++ ex02/src/ShrubberyCreationForm.hpp
@@ -3,26 +3,21 @@
 #include <iostream>
 #include <string>
 #include "Form.hpp"
- 
-// Class definition 
+
+
 class ShrubberyCreationForm : public Form
 {
 private: 
 	std::string _target;
  
-public: 
-// Constructor 
+public:
 	ShrubberyCreationForm(); 
 	ShrubberyCreationForm( const ShrubberyCreationForm &rhs); 
 	ShrubberyCreationForm(std::string newTarget);
 	~ShrubberyCreationForm(); 
-	ShrubberyCreationForm& operator=( const ShrubberyCreationForm &rhs); 
-// Getters 
-	std::string getTarget(); 
-// Setters 
-	void setTarget(std::string _target); 
-// Output 
-	void output(); 
+	ShrubberyCreationForm& operator=( const ShrubberyCreationForm &rhs);
+	std::string getTarget();
+	void setTarget(std::string _target);
 }; 
  
 #endif
